21742a3094f0e64a4f08e6f8a2a24ec083ab49eb
mrayushmehrotra <ayushmehrotra099@gmail.com>
2024-05-28 12:57:17 +0530

    added regex for video code | and srt file

--- backend/index.py
+++ backend/index.py
@@ -1,9 +1,22 @@
+#imports 
+from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
+import re 
 
-srt = YouTubeTranscriptApi.get_transcript("SW14tOda_kI")
+#enter the url and extract the necessary things
+url = input("Enter the Youtube Video/Podcast")
+pattern_for_yt_videoId = r"[?&]v=([^&]+)"
+match = re.search(pattern_for_yt_videoId, url)
+if match:
+    video_id = match.group(1)
+else:
+    print("Please enter a valid or complete YT Video url")
 
+#downloading SRT file  and extracting Subs only
+srt = YouTubeTranscriptApi.get_transcript(video_id)
 text_only = [item['text'] for item in srt] 
-
 result = " ".join(text_only)
-
-print(result)
+pattern_for_bracketed_text =  r"\[.*?\]" # to remove [Music] and etc. in srt file
+cleaned_text = re.sub(pattern_for_bracketed_text, "", result)
+cleaned_text = cleaned_text.strip() 
+print(cleaned_text)
